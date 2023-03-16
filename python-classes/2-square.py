1042f588b20d5742513494cb2460b0f6650375b8
Mgeni-E <e.mgeni@alustudent.com>
2023-03-16 10:32:06 -0700

    dhhdhds

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Its crazy that we are not importing any modules"""
+"""Define a class Square."""
+
+
 class Square:
-    """Class is documented"""
-    
+    """Represent a square."""
     def __init__(self, size=0):
-        """Initialize a new instance of the Square class."""
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
+        self.__size = size
