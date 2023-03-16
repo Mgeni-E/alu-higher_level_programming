f4eef82eff2122f066e176772bfab269f79e1b72
Mgeni-E <e.mgeni@alustudent.com>
2023-03-16 04:19:50 -0700

    dgdjwa

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Its crazy that we are not importing any modules"""
 class Square:
-    """Its crazy that we are not importing any modules"""
+    """Class is documented"""
     
     def __init__(self, size=0):
         """Initialize a new instance of the Square class."""
