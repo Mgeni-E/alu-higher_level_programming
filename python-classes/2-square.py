46194b8f96612d46afde39bf6007fb9e614a8005
Mgeni-E <e.mgeni@alustudent.com>
2023-03-16 04:13:32 -0700

    dgdjwa

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+class Square:
+    """Its crazy that we are not importing any modules"""
+    
+    def __init__(self, size=0):
+        """Initialize a new instance of the Square class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
