648822367275cb14d64d6d39c4af18b6b6f92293
Mgeni-E <e.mgeni@alustudent.com>
2023-06-16 03:42:57 -0700

    Initial commit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv.slice(2);
-
-fs.writeFile(file[0], file[1], 'utf8', (err) => {
-  if (err) {
-    console.log(err);
+const file = process.argv[2];
+const write = process.argv[3];
+fs.writeFile(file, write, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
   }
 });
